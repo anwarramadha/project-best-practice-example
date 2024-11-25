33561e9c31bd2126495eb8232dfacba5de24e464
Anwar Ramadha <anwarramadha22@gmail.com>
2024-11-25 14:02:51 +0700

    test: :white_check_mark: added a new project to handle common components test

---
+++ apps/common-components-test/nuxt.config.ts
@@ -0,0 +1,87 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import dotEnvExtended from 'dotenv-extended'
+import viteCompression from 'vite-plugin-compression'
+
+dotEnvExtended.load({
+  path: '../../.env',
+  defaults: '../../.env',
+  schema: '../../.env.schema'
+})
+
+interface ViteProxyOptions {
+  target: string;
+  changeOrigin: boolean;
+  rewrite: (path: string) => string;
+}
+
+interface ViteServerOptions {
+  fs: {
+    allow: string[];
+  };
+  proxy: {
+    [key: string]: ViteProxyOptions;
+  };
+}
+
+interface ViteOptions {
+  server: ViteServerOptions;
+  plugins: any[];
+}
+
+interface NuxtConfig {
+  compatibilityDate: string;
+  devtools: { enabled: boolean };
+  modules: string[];
+  tailwindcss: object;
+  vite: ViteOptions;
+}
+
+console.log('API BASE URL',process.env.API_BASE_URL)
+
+export default defineNuxtConfig({
+  ssr: true,
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@sutekitechid/project-best-practices-example',
+    '@nuxt/test-utils/module'
+  ],
+  tailwindcss: {},
+  vite: {
+    server: {
+      fs: {
+        allow: [
+          '..', '../..'
+        ]
+      },
+      proxy: {
+        '/api': {
+          target: process.env.API_BASE_URL as string,
+          changeOrigin: true,
+          rewrite: (path: string) => path.replace(/^\/api/, '')
+        },
+        '/multisite-config': {
+          target: process.env.MULTISITE_CONFIG_URL as string,
+          changeOrigin: true,
+          rewrite: (path: string) => path.replace(/^\/multisite-config/, '')
+        }
+      }
+    },
+    plugins: [
+      viteCompression({
+        algorithm: 'brotliCompress'
+      })
+    ]
+  },
+
+  runtimeConfig: {
+    public : {
+      API_BASE_URL: process.env.API_BASE_URL,
+      MULTISITE_CONFIG_URL: process.env.MULTISITE_CONFIG_URL,
+      MULTISITE_NODE_ID: process.env.MULTISITE_NODE_ID,
+      MULTISITE_SERVICE_ID: process.env.MULTISITE_SERVICE_ID || 'pmb'
+    },
+    API_KEY: process.env.API_KEY
+  }
+} as NuxtConfig)
